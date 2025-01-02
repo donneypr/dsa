6636da9ea75c0a4fb83e532ad1d3262ae7f24947
donneypr <donatoprabahar@gmail.com>
2025-01-02 16:28:35 -0500

    added longestcommonprefix

---
+++ longestcommonprefix.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return ""; 
+        }
+        String outputStr = "";
+        for (int i = 0; i < strs[0].length(); i++) { 
+            char currentChar = strs[0].charAt(i);   
+            for (int j = 1; j < strs.length; j++) { 
+                if (i >= strs[j].length() || strs[j].charAt(i) != currentChar) {
+                    return outputStr; 
+                }
+            }
+            outputStr += currentChar; 
+        }
+        return outputStr; 
+    }
+}
